3adcc7dfd77eeb5a43b6f07a848009f9abf68809
Kareem Rashed <kareemrashed@aucegypt.edu>
2025-08-25 15:28:50 +0300

    Initial Commit

---
+++ SebhaNew/SebhaNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SebhaNewApp.swift
+//  SebhaNew
+//
+//  Created by Kareem Rashed on 25/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct SebhaNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SebhaNewTests/SebhaNewTests.swift
@@ -0,0 +1,17 @@
+//
+//  SebhaNewTests.swift
+//  SebhaNewTests
+//
+//  Created by Kareem Rashed on 25/08/2025.
+//
+
+import Testing
+@testable import SebhaNew
+
+struct SebhaNewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
